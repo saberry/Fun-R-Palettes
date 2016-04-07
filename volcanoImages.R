b7800c6e8bebd27b646acec2425ef85176e4a1c8
Seth Berry <seth.berry@nd.edu>
2016-04-07 09:31:33 -0400

    Adding images

---
+++ volcanoImages.R
@@ -0,0 +1,27 @@
+image(volcano, col = funPals[["spBoys"]], main = "South Park Boys")
+
+image(volcano, col = funPals[["spKenny"]], main = "Kenny")
+
+image(volcano, col = funPals[["spStan"]], main = "Stan")
+
+image(volcano, col = funPals[["spKyle"]], main = "Kyle")
+
+image(volcano, col = funPals[["spCartman"]], main = "Cartman")
+
+image(volcano, col = funPals[["spcredits"]], main = "Credits")
+
+image(volcano, col = funPals[["boem"]], main = "Brotherhood of Mutants")
+
+image(volcano, col = funPals[["xmen1"]], main = "X-Men 1")
+
+image(volcano, col = funPals[["xmen2"]], main = "X-Men 2")
+
+image(volcano, col = funPals[["marvel"]], main = "Marvel Group")
+
+image(volcano, col = funPals[["turboKid1"]], main = "Turbo Kid 1")
+
+image(volcano, col = funPals[["turboKid2"]], main = "Turbo Kid 2")
+
+image(volcano, col = funPals[["turboKid3"]], main = "Turbo Kid 3")
+
+image(volcano, col = funPals[["beavisButthead"]], main = "Beavis and Butthead")
